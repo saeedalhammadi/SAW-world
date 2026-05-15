204f5551aae020c6450a3aae82bea661955d2d32
saeedalhammadi <alhammadi78@gmail.com>
2026-05-16 03:58:15 +0400

    Quartz sync: May 16, 2026, 3:58 AM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,7 +15,7 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          <a href="https://sawinstory.com">SAEED {81554}</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
